feabe260f95c7b84dd6b6fbbc8eef2e981a763b6
cahaug <caleahaug@gmail.com>
2020-02-26 18:59:07 -0700

    passing test on bubble_sort and selection_sort now

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,9 @@
-# arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+
+# Selection Sort: (imagine you are holding n cards in your hand...)
+# 1. Select the smallest card and move it to the far left
+# 2. Select the next smallest card and move it to the left so that it is to the immediate right of the previous card
+# 3. Repeat the previous step until all the cards in the right place
 
 def selection_sort( arr ):
     # loop through n-1 elements
@@ -6,10 +11,11 @@ def selection_sort( arr ):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
+        # (STEP #1)
         absolute_minimum = min(arr)
         arr.remove(absolute_minimum)
         arr.insert(0, absolute_minimum)
-        # (hint, can do in 3 loc) 
+        # (hint, can do in 3 loc) (STEP #2,3)
         for j in range(1, len(arr)):
             local_minimum = min(arr[j:])
             arr.remove(local_minimum)
@@ -22,13 +28,31 @@ def selection_sort( arr ):
 
 # print(selection_sort(arr1))
 
+
+# Bubble Sort: (imagine you are holding n cards in your hand...)
+# 1. Compare the first pair of elements 
+#       - If the right hand side is less than the left hand side, swap the values
+#       - Else, do nothing
+# 2. Iterate through the array, comparing other pairs of adjacent elements
+# 3. If one or more swaps was performed on a given pass, repeat the pass/process (reset swap boolean/counter here)
+
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    arrLen = len( arr )
+    for i in range(arrLen):
+        for j in range(0, arrLen-i-1):
+#           (STEP #1,2,3)
+            if arr[j]>arr[j+1]:
+                comparative_maximum = arr[j]
+                arr.remove(comparative_maximum)
+                arr.insert(j+1, comparative_maximum)
     return arr
 
+print(bubble_sort(arr1))
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+# print(count_sort(arr1))
