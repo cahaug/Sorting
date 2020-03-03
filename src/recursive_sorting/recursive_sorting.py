9e1c25a5bf3756ce12f39f392160cafd7e3829db
cahaug <caleahaug@gmail.com>
2020-03-02 19:49:22 -0700

    refactored with code from lecture

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -4,81 +4,46 @@ arrD = [3,4]
 arrE = [1]
 arrF = [1,2,3,4,5,6]
 # TO-DO: complete the helper function below to merge 2 sorted arrays
+# given two sorted arrays, return a new combined array 
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
-    for i in range(0, elements):
-        if arrA == [] and len(arrB) > 0:
-            merged_arr.pop()
-            print('arrA is empty')
-            merged_arr.insert(i, arrB[0])
-            arrB.pop(0)
-        elif arrB == [] and len(arrA) > 0:
-            merged_arr.pop()
-            print('arrB is empty')
-            merged_arr.insert(i, arrA[0])
-            arrA.pop(0)
-        elif arrA[0] <= arrB[0]:
-            merged_arr.pop()
-            print('A<B')
-            merged_arr.insert(i, arrA[0])
-            arrA.pop(0)
-        elif arrA[0] > arrB[0]:
-            merged_arr.pop()
-            print('A>B')
-            merged_arr.insert(i, arrB[0])
-            arrB.pop(0)
-        elif len(arrA) == 0 and len(arrB) == 0:
-            print("""We're done here.""")
+    merged_arr = []
+    i = 0
+    j = 0
+    while i < len(arrA) and j < len(arrB):
+        # compare the two values, choose the smaller one
+        if (arrA[i] <= arrB[j]):
+            merged_arr.append(arrA[i])
+            i+=1
         else:
-            print("""Ran Out of Conditions. Error?""")
-
-
-    # print(merged_arr)
-    return merged_arr
+            merged_arr.append(arrB[j])
+            j+=1
+    return merged_arr + arrA[i:] + arrB[j:]
 
 # print(merge(arrA,arrB))
-
-# this function takes any array of numbers and 
-# divides it in half, into two arrays. if odd number of values the +1 goes to the front array
-def splitArr( arrA ):
-    if len( arrA ) == 1:
-        return [arrA]
-    elif len(arrA) == 2:
-        newArray1= [arrA[0]]
-        newArray2= [arrA[1]]
-        return [newArray1, newArray2]
-    elif len(arrA) % 2 != 0:
-        midpoint_idx = len(arrA) // 2 + 1
-        newArray1 = arrA[:int(midpoint_idx)]
-        newArray2 = arrA[int(midpoint_idx):]
-        return [newArray1, newArray2]
-    elif len(arrA) % 2 == 0:
-        midpoint_idx = len(arrA) // 2
-        newArray1 = arrA[:int(midpoint_idx)]
-        newArray2 = arrA[int(midpoint_idx):]
-        return [newArray1, newArray2]
 
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
+# the job of this function is to return a sorted array
 def merge_sort( arr ):
     # TO-DO
-    # while we have arrays that are greater than 1 in length, split the array
-    #  into two subarrays, each half the length of the original
-    splitOnce = splitArr( arr )
-    arraysShortEnough = False
-    shortEnoughArrays = [] 
-    while arraysShortEnough != True
-        for array in splitOnce:
-            if len( array ) == 1:
-                shortEnoughArrays.append(array)
-            else:
-                return splitArr( array )
-             
+    # base case (Easiest case)
+    if len(arr) <= 1:
+        return arr
 
-    return arr
+    # divide the arrays
+    half = len(arr) // 2
+    left = merge_sort(arr[0:half])
+    right = merge_sort(arr[half:])
+
+        
+
+    ##### SORTING #####
+    # somehow, the arrays get sorted, because 
+    # we are taking arrays of length 1,
+    # which means it's already sorted *greencheckmark*
+    return merge(left, right)
+    # merging sorted arrays
 
 
 # STRETCH: implement an in-place merge sort algorithm
